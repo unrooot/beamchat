5734a600811085cb4bc0f942bcc2b6a4894eb366
unrooot <nweeek@gmail.com>
2020-05-12 01:02:06 -0700

    corrected chatbar to support expected ux

--- src/ReplicatedStorage/modules/chatmodule.lua
+++ src/ReplicatedStorage/modules/chatmodule.lua
@@ -27,93 +27,101 @@ lib.search = function()
 	local input = chatbar.input.Text
 	local lastWord
 
-	for result in string.gmatch(input, "[^%s]+") do
-		if string.sub(input, (#input - #result) + 1) == result then
-			lastWord = result
-		end
-	end
-
-	if string.sub(input, 0, 2) ~= "--" and string.sub(lastWord, 0, 1) ~= ":" then
-		-- search for players
-		chatbar.input:ReleaseFocus(false)
-
-		local matches = {}
+	if input ~= "" and input ~= nil then
 		for result in string.gmatch(input, "[^%s]+") do
 			if string.sub(input, (#input - #result) + 1) == result then
-				-- strip @ from string
-				local name = string.lower(string.gsub(result, "@", ""))
-				for _,player in pairs(game.Players:GetPlayers()) do
-					if string.find(string.lower(player.Name), name) then
-						table.insert(matches, player.Name)
+				lastWord = result
+			end
+		end
+
+		if not lastWord then
+			lastWord = ""
+		end
+
+		if string.sub(input, 0, 2) ~= "--" and string.sub(lastWord, 0, 1) ~= ":" then
+			-- search for players
+			chatbar.input:ReleaseFocus(false)
+
+			local matches = {}
+			for result in string.gmatch(input, "[^%s]+") do
+				if string.sub(input, (#input - #result) + 1) == result then
+					-- strip @ from string
+					local name = string.lower(string.gsub(result, "@", ""))
+					for _,player in pairs(game.Players:GetPlayers()) do
+						if string.find(string.lower(player.Name), name) then
+							table.insert(matches, player.Name)
+						end
 					end
 				end
 			end
-		end
 
-		if #matches > 1 then
-			local resultsFrame = Instance.new("Frame", chatbar)
-			resultsFrame.AnchorPoint = Vector2.new(0, 1)
-			resultsFrame.BackgroundColor3 = c3()
-			resultsFrame.BorderSizePixel = 0
-			resultsFrame.BackgroundTransparency = 0.5
-			resultsFrame.Name = "results"
-			resultsFrame.Position = u2(0, chatbar.input.TextBounds.X, 0, 0)
+			if #matches > 1 then
+				local resultsFrame = Instance.new("Frame", chatbar)
+				resultsFrame.AnchorPoint = Vector2.new(0, 1)
+				resultsFrame.BackgroundColor3 = c3()
+				resultsFrame.BorderSizePixel = 0
+				resultsFrame.BackgroundTransparency = 0.5
+				resultsFrame.Name = "results"
+				resultsFrame.Position = u2(0, chatbar.input.TextBounds.X, 0, 0)
 
-			local highlight = Instance.new("Frame", resultsFrame)
-			highlight.BackgroundTransparency = 0.85
-			highlight.BackgroundColor3 = c3(255, 255, 255)
-			highlight.BorderSizePixel = 0
-			highlight.ZIndex = 2
-			highlight.Size = u2(1, 0, 0, 26)
-			highlight.Position = u2(0, 0, 0, 0)
-			highlight.Name = "highlight"
+				local highlight = Instance.new("Frame", resultsFrame)
+				highlight.BackgroundTransparency = 0.85
+				highlight.BackgroundColor3 = c3(255, 255, 255)
+				highlight.BorderSizePixel = 0
+				highlight.ZIndex = 2
+				highlight.Size = u2(1, 0, 0, 26)
+				highlight.Position = u2(0, 0, 0, 0)
+				highlight.Name = "highlight"
 
-			local entries = Instance.new("Frame", resultsFrame)
-			entries.BorderSizePixel = 0
-			entries.BackgroundTransparency = 1
-			entries.Size = u2(1, 0, 1, 0)
-			entries.Name = "entries"
+				local entries = Instance.new("Frame", resultsFrame)
+				entries.BorderSizePixel = 0
+				entries.BackgroundTransparency = 1
+				entries.Size = u2(1, 0, 1, 0)
+				entries.Name = "entries"
 
-			local padding = Instance.new("UIPadding", entries)
-			padding.PaddingLeft = UDim.new(0, 5)
-			padding.PaddingRight = UDim.new(0, 5)
+				local padding = Instance.new("UIPadding", entries)
+				padding.PaddingLeft = UDim.new(0, 5)
+				padding.PaddingRight = UDim.new(0, 5)
 
-			local listLayout = Instance.new("UIListLayout", entries)
-			listLayout.SortOrder = Enum.SortOrder.LayoutOrder
+				local listLayout = Instance.new("UIListLayout", entries)
+				listLayout.SortOrder = Enum.SortOrder.LayoutOrder
 
-			local longest = ""
-			for i = 1, #matches do
-				if #matches[i] > #longest then
-					longest = matches[i]
+				local longest = ""
+				for i = 1, #matches do
+					if #matches[i] > #longest then
+						longest = matches[i]
+					end
+
+					local t = Instance.new("TextLabel", entries)
+					t.BackgroundTransparency = 1
+					t.BorderSizePixel = 0
+					t.ZIndex = 3
+					t.Size = u2(1, 0, 0, 26)
+					t.TextXAlignment = Enum.TextXAlignment.Left
+					t.Font = Enum.Font.GothamSemibold
+					t.Name = tostring(i)
+					t.Text = matches[i]
+					t.TextSize = 14
+					t.TextColor3 = c3w
+					t.TextTransparency = i == 1 and 0 or 0.2
 				end
 
-				local t = Instance.new("TextLabel", entries)
-				t.BackgroundTransparency = 1
-				t.BorderSizePixel = 0
-				t.ZIndex = 3
-				t.Size = u2(1, 0, 0, 26)
-				t.TextXAlignment = Enum.TextXAlignment.Left
-				t.Font = Enum.Font.GothamSemibold
-				t.Name = tostring(i)
-				t.Text = matches[i]
-				t.TextSize = 14
-				t.TextColor3 = c3w
-				t.TextTransparency = i == 1 and 0 or 0.2
+				resultsFrame:TweenSize(u2(0, txt:GetTextSize(longest, 14, Enum.Font.GothamSemibold, Vector2.new(185, 20)).X + 10, 0, #matches*26), "Out", "Quart", 0.25, true)
+				lib.searching = {type = "username", selected = 1, results = matches, last = lastWord}
+			elseif #matches == 1 then
+				chatbar.input.Text = string.sub(input, 0, #input - #lastWord) .. matches[1] .. " "
 			end
-
-			resultsFrame:TweenSize(u2(0, txt:GetTextSize(longest, 14, Enum.Font.GothamSemibold, Vector2.new(185, 20)).X + 10, 0, #matches*26), "Out", "Quart", 0.25, true)
-
-			lib.searching = {type = "username", selected = 1, results = matches, last = lastWord}
-		elseif #matches == 1 then
-			chatbar.input.Text = string.sub(input, 0, #input - #lastWord) .. matches[1] .. " "
+		elseif string.sub(lastWord, 0, 1) == ":" then
+			print("searching emoji")
+		elseif string.sub(input, 0, 2) == "--" then
+			print("searching command")
 		end
 
 		wait()
 		chatbar.input:CaptureFocus()
-	elseif string.sub(lastWord, 0, 1) == ":" then
-		print("searching emoji")
-	elseif string.sub(input, 0, 2) == "--" then
-		print("searching command")
+	else
+		wait()
+		chatbar.input:CaptureFocus()
 	end
 end
 
@@ -129,8 +137,8 @@ end
 
 -- toggling the chatbar/sending messages
 lib.chatbar = function(sending)
-	lib.chatbarToggle = not lib.chatbarToggle
-	if lib.chatbarToggle then
+	if not lib.chatbarToggle then
+		lib.chatbarToggle = true
 		-- prepare chatbar
 		chatbar.input.Text = ""
 
@@ -144,6 +152,7 @@ lib.chatbar = function(sending)
 		game:GetService("RunService").RenderStepped:wait()
 		chatbar.input:CaptureFocus()
 	else
+		lib.chatbarToggle = false
 		-- capture user input
 		local msg = chatbar.input.Text
 		chatbar.input.Text = ""
