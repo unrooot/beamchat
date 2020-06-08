41bea9d25d80c198ee73a91c1b2a8696ea46ca51
unrooot <nweeek@gmail.com>
2020-06-08 15:47:47 -0700

    added function to fetch chat color

--- src/ReplicatedStorage/modules/chatcolors.lua
+++ src/ReplicatedStorage/modules/chatcolors.lua
@@ -1,5 +1,12 @@
+-- mostly yoinked from the core chat
+
+local byte = string.byte
+local sub = string.sub
+
 local c3 = Color3.fromRGB
-local colors = {
+
+local colors = {}
+local chatColors = {
 	c3(211, 84, 84),	-- red
 	c3(82, 172, 255),	-- blue
 	c3(73, 202, 95),	-- green
@@ -9,5 +16,25 @@ local colors = {
 	c3(255, 126, 182),	-- pink
 	c3(211, 183, 159)	-- tan
 }
+
+function colors.getColor(username)
+	local value = 0
+	for i = 1, #username do
+		local cv = byte(sub(username, i, i))
+		local ri = #username - i + 1
+
+		if #username % 2 == 1 then
+			ri = ri - 1
+		end
+
+		if ri % 4 >= 2 then
+			cv = -cv
+		end
+
+		value = value + cv
+	end
+
+	return chatColors[(value % #chatColors) + 1]
+end
 
 return colors
