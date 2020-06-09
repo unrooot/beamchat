36488f129622be046abc8cbcce34e36b4eea0b53
unrooot <nweeek@gmail.com>
2020-06-09 01:55:26 -0700

    client configuration file

---
+++ src/ReplicatedStorage/modules/clientConfig.lua
@@ -0,0 +1,9 @@
+local config = {
+	-- The amount of messages that will show up before they get cleared.
+	chatLimit = 300;
+
+	-- The chat animation type. "modern" for a tween/fade, classic for just a fade.
+	chatAnimation = "modern";
+}
+
+return config
