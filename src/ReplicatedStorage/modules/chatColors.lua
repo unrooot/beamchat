779228762417001bde1317eb4bc6bef5e434d6bd
unrooot <nweeek@gmail.com>
2020-07-09 00:37:42 -0700

    converted c3 to hex

--- src/ReplicatedStorage/modules/chatColors.lua
+++ src/ReplicatedStorage/modules/chatColors.lua
@@ -1,21 +1,32 @@
 -- mostly yoinked from the core chat
 
+local colors = {}
+local chatColors = {
+	"#d35454", -- red
+	"#52acff", -- blue
+	"#49ca5f", -- green
+	"#bc69eb", -- purple
+	"#ff8661", -- orange
+	"#fdff50", -- yellow
+	"#ff7eb6", -- pink
+	"#d3b79f"  -- tan
+}
+
 local byte = string.byte
 local sub = string.sub
 
-local c3 = Color3.fromRGB
-
-local colors = {}
-local chatColors = {
-	c3(211, 84, 84),	-- red
-	c3(82, 172, 255),	-- blue
-	c3(73, 202, 95),	-- green
-	c3(188, 105, 235),	-- purple
-	c3(255, 134, 97),	-- orange
-	c3(253, 255, 80),	-- yellow
-	c3(255, 126, 182),	-- pink
-	c3(211, 183, 159)	-- tan
-}
+-- color3 values
+-- local c3 = Color3.fromRGB
+-- local chatColors = {
+-- 	c3(211, 84, 84),	-- red
+-- 	c3(82, 172, 255),	-- blue
+-- 	c3(73, 202, 95),	-- green
+-- 	c3(188, 105, 235),	-- purple
+-- 	c3(255, 134, 97),	-- orange
+-- 	c3(253, 255, 80),	-- yellow
+-- 	c3(255, 126, 182),	-- pink
+-- 	c3(211, 183, 159)	-- tan
+-- }
 
 function colors.getColor(username)
 	local value = 0
