966b9f84209bebdb8b0565e9201201a75ba25cc8
unrooot <nweeek@gmail.com>
2020-07-24 03:22:02 -0700

    bugfix

--- src/ServerScriptService/commands/to.lua
+++ src/ServerScriptService/commands/to.lua
@@ -1,5 +1,5 @@
 return function(plr, targets)
 	if targets[1] then
-		plr.Character.HumanoidRootPart.CFrame = targets[1].Character.CFrame
+		plr.Character.HumanoidRootPart.CFrame = targets[1].Character.HumanoidRootPart.CFrame
 	end
 end
