60aa167afbb0dc938f45ddc91752b69149d37510
unrooot <nweeek@gmail.com>
2020-08-13 21:20:44 -0700

    reduced default chat limit + reworded help message

--- src/ReplicatedStorage/modules/clientConfig.lua
+++ src/ReplicatedStorage/modules/clientConfig.lua
@@ -1,12 +1,12 @@
 local config = {
 	-- The amount of messages that will show up before they get cleared.
-	chatLimit = 300;
+	chatLimit = 150;
 
 	-- The chat animation type. "modern" for a tween/fade, classic for just a fade.
 	chatAnimation = "modern";
 
 	-- The help message that appears when typing /? or /help.
-	helpMessage = "beamchat2, by moonbeam (v2.2) ————— /emojis - See the list of custom emojis. ————— /mute {plr} or /unmute {plr} ————— Mute/unmute a player. ————— /mutelist - See the players who you have muted. ————— /w {plr} {msg} - Whisper to a player. ————— :emoji: - Search for emojis. ————— (desktop) TAB key - Autocomplete usernames.";
+	helpMessage = "beamchat2, by moonbeam (v2.3) [/emojis] - See the list of custom emojis. [/mute {plr} or /unmute {plr}] - Mute/unmute a player. [/mutelist] - See the players who you have muted. [/w {plr} {msg}] - Whisper to a player. [:emoji:] - Search for emojis. [(desktop) TAB key] - Autocomplete usernames.";
 }
 
 return config
