748e15213aa38f867d7168825829248dfbf90092
unrooot <nweeek@gmail.com>
2020-08-13 21:09:47 -0700

    cleaned up animation file

--- src/ReplicatedStorage/emoteAnimations/widepeepoHappy.lua
+++ src/ReplicatedStorage/emoteAnimations/widepeepoHappy.lua
@@ -1,6 +1,4 @@
-local rs = game:GetService("ReplicatedStorage")
 local spr = require(script.Parent.Parent:WaitForChild("modules"):WaitForChild("spr"))
-
 local u2 = UDim2.new
 
 local function keyframes(gui)
