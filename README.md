e53f26538fb787feb14fe57fe5f19cc0cb7e70ba
unrooot <64768843+unrooot@users.noreply.github.com>
2021-01-05 16:47:37 -0700

    nuke

--- README.md
+++
@@ -1,6 +0,0 @@
-# beamchat2
-An open-source remake of the default Roblox chat, but better.
-
-This project is nowhere near completion.
-
-###### Have you heard of beamchat2?
--- src/ServerScriptService/modules/chatTags.lua
+++
@@ -1,9 +0,0 @@
--- This file contains all of the special chat tags. The format goes like this:
--- {"chat tag", {user ids who will receive it}}
-
-return {
-	{
-		"{#7e6958}{b}[{b}{b}{#ccc4b3}dev{b}{b}{#7e6958}]{b}{} ",
-		{}
-	}
-}
